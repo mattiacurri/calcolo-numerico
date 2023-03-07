8bcedce3c73484534cb0028f89dd4fa323631f78
mattiacurri <mattia2002@gmail.com>
2023-03-07 21:06:53 +0100

    Nuovi test

--- test_zeri.py
+++ test_zeri.py
@@ -1,8 +1,8 @@
 from zeri import *
 
-x, y = zeros(f, 0, pi/2, 1e-15, 100, "rel")
-z, u = zeros(f, 0, pi/2, 1e-15, 100, "mix") 
-print(x, y)
-print(z, u)
-# print(zeros_er(f, 0, pi/2)
-print(f(x), f(z))
+x, y = zeros(f, 0, pi/2, 1e-15, 100, "abs")
+print(f"Zero: {x}, Passi: {y}")
+x, y = zeros(f, 0, pi/2, 1e-15, 100, "mix") 
+print(f"Zero: {x}, Passi: {y}")
+x, y = zeros(h, 0, 10, 1e-15, 100, "abs") 
+print(f"Zero: {x}, Passi: {y}")
