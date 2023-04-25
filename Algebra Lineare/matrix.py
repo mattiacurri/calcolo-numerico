6a2ac10e9c4723d73b8ce32ed17d1bacd2008a6d
mattiacurri <mattia2002@gmail.com>
2023-04-25 19:38:41 +0200

    Implementato il prodotto matriciale

--- Algebra Lineare/matrix.py
+++ Algebra Lineare/matrix.py
@@ -10,8 +10,22 @@ def sum_matrix(A: array, B: array):
         for j in range(0, n):
             C[i][j] = A[i][j] + B[i][j]
     return C
-A = random.randint(20, size=(4, 4))
+
+def dot_matrix(A: array, B: array):
+    [m, n] = shape(A)
+    [r, c] = shape(B)
+    if n != r:
+        raise ValueError("Dimensioni non compatibili")
+    C = zeros(shape = (n, r))
+    for i in range(0, m):
+        for j in range(0, c):
+            for k in range(0, n):
+                C[i][j] += A[i][k] * B[k][j]
+    return C
+
+A = random.randint(2, 5, size=(3, 3))
 print(A)
-B = random.randint(20, size=(4, 4))
+B = random.randint(2, 5, size=(3, 3))
 print(B)
 print(sum_matrix(A, B))
+print(dot_matrix(A, B))
