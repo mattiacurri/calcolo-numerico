46b108f55ccce171eacace6635a589e31d188fb0
mattiacurri <mattia2002@gmail.com>
2023-04-27 17:56:45 +0200

    Implementato l'algoritmo di sostituzione all'indietro

--- Algebra Lineare/matrix.py
+++ Algebra Lineare/matrix.py
@@ -42,8 +42,24 @@ def is_triang_up(A):
                     break
     return is_triang
 
-A = array([[1, 2, 3], [0, 5, 6], [0, 0, 9]])
+def solve_triup(A, b):
+    if not is_triang_up(A):
+        raise ValueError("La matrice non e' triangolare superiore")
+    n = len(A)
+    x = zeros(shape=(n, 1))
+    for i in range(n - 1, -1, -1):
+        if abs(A[i][i]) < 1e-15:
+            raise ValueError("Matrice singolare")
+        part_sum = 0
+        for j in range(i + 1, n):
+            part_sum += A[i][j] * x[j]
+        x[i] = (b[i] - part_sum)/A[i][i]
+    return x
+
+A = array([[1, 2, 3, 4], [0, 3, 1, 3], [0, 0, 9, 4], [0, 0, 0, 4]])
 print(A)
 B = random.randint(2, 5, size=(3, 3))
 print(B)
 print(is_triang_up(A))
+b = array([4, 5, 6, 7])
+print(solve_triup(A, b))
