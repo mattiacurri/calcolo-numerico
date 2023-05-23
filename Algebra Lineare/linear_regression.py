8b24550baf53a39969ba918a5dbdedd690ff07ca
mattiacurri <mattia2002@gmail.com>
2023-05-23 18:29:05 +0200

    Improvements

--- Algebra Lineare/linear_regression.py
+++ Algebra Lineare/linear_regression.py
@@ -4,10 +4,13 @@ from matplotlib.pyplot import *
 import warnings
 
 def regression(x, y, yp, index):
+    if index > 1 or index < 0:
+        print("L'indice di determinazione deve essere compreso tra 0 e 1")
+        return
     index_found = 0
     i = 0
     while index_found < index:
-        xx = linspace(-10, 10, 10000)
+        xx = linspace(-100, 100, 1000000)
         with warnings.catch_warnings():
             warnings.filterwarnings('error')
             try:
@@ -21,12 +24,16 @@ def regression(x, y, yp, index):
         i += 1
     plot(xx, pxx)
     plot(x, y, 'ro', x, yp, 'bo', markersize=4)
+    legend(['Polinomio di grado ' + str(i - 1)])
+    title('Approssimazione polinomiale di grado ' + str(i - 1) + ' con indice di determinazione ' + str(index_found))
+    resize = get_current_fig_manager()
+    resize.resize(1000, 600)
     show()
-    return i
+    return i - 1
     
     
-m = 11 # numero di punti
-x = linspace(-10, 10, m) # vettore delle ascisse
-y = 1/(1 + x**2) # funzione di Runge 
+m = 50 # numero di punti
+x = linspace(-100, 100, m) # vettore delle ascisse
+y = -x**3 + 2 * x**2 - 1 + x
 yp = y + 0.021 * random.randn(m) # simulo la distorsione dei dati con un rumore gaussiano di media 0 e varianza 0.021
-print(regression(x, y, yp, 0.7))
+print(regression(x, y, yp, 0.9))
